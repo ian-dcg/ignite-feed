58ddd23c010f5a2309351327e44787cc4fa27693
Ian Costa <ianndcg@gmail.com>
2022-08-29 10:03:55 -0300

    Projeto concluído

---
+++ src/App.tsx
@@ -0,0 +1,85 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
+
+interface Posts {
+  id: number;
+  author: {
+    name: string;
+    role: string;
+    avatarUrl: string;
+  };
+  content: {
+    type: 'paragraph' | 'link';
+    content: string;
+  }[];
+  publishedAt: Date;
+}
+
+const posts: Posts[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/ian-dcg.png',
+      name: 'Ian da Costa',
+      role: 'Full Stack Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'ian.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-27 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'ian.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-27 22:00:00'),
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import { PencilSimpleLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/ian-dcg.png" />
+        <strong>Ian da Costa</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
